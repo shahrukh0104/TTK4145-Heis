55f5c8966e7ed90bdcd0db6556928ba5d37b6c83
Student <student@itk-d650>
2016-02-05 14:10:34 +0100

    Day 3 finsihed, Something wrong with TCP

--- Oving 3/tcpProtocol.go
+++ Oving 3/tcpProtocol.go
@@ -3,47 +3,51 @@ package main
 import (
 	"fmt"
 	"net"
-	"time"
 )
-
 
 const (
-	HOST = 20016
-	PORT = 34933
+	HOST = "20016"
+	PORT = "33546"
 )
-
 
 func TCPserver() {
 
-	fmt.Println("Listening for TCP connection on " + HOST + ":" + PORT)
-	addr, _ := net.ResolveUDPAddr("udp", ":"+ PORT)
+	fmt.Println("Listening for TCP connection on " + "30000" + ":" + "20016")
+	addr, _ := net.ResolveTCPAddr("tcp", PORT)
 	tcpListenSocket, err := net.ListenTCP("tcp", addr)
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	}
 	defer tcpListenSocket.Close()
 	for {
-		tcpConnectSocket , err := tcpListenSocket.AcceptTCP()
+		tcpConnectSocket, err := tcpListenSocket.AcceptTCP()
 		defer tcpConnectSocket.Close()
 
-		if err != nil{
+		if err != nil {
 			fmt.Println(err)
 		}
-		go tcpHandleSomethingSomething(tcpConnectSocket)	
+		go tcpHandleSomethingSomething(tcpConnectSocket)
 
 	}
 }
 
-
-
-
-
-
 func TCPclient() {
 	fmt.Println("Connect to this socket: 129.241.187.147")
-	addr, err := net.ResolveTCPAddr("tcp", 129.241.187.147:20016)
-	tcpConnectSocket, err := net.DialTCP("tcp", addr)
+	tcpConnectSocket, err := net.Dial("tcp", "129.241.187.147:33546")
 
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	
+	tcpConnectSocket.Write([]byte("Connect to: 129.241.187.147:33546\x00"))
+}
+
+func tcpHandleSomethingSomething(socket *net.TCPConn) {
+	fmt.Println("message recieved")
+}
+
+func main() {
+	TCPserver()
+	TCPclient()
+
 }
