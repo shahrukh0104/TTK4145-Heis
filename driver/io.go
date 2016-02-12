aa2915888cd9ae310856235b14325cd57f901648
Student <student@itk-d650>
2016-02-12 16:15:48 +0100

    io.go is translated

--- driver/io.go
+++ driver/io.go
@@ -1,7 +1,32 @@
-package driver  // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.h and driver.go
+package driver // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.h and driver.go
 /*
 #cgo CFLAGS: -std=c11
 #cgo LDFLAGS: -lcomedi -lm
 #include "io.h"
 */
 import "C"
+
+func io_init() {
+	C.io_init()
+}
+
+func io_set_bit(channel int) {
+	C.io_set_bit(C.int(channel))
+
+}
+
+func io_clear_bit(channel int) {
+	C.io_clear_bit(C.int(channel))
+}
+
+func io_write_analog(channel int, value int) {
+	C.io_write_analog(C.int(channel), C.int(value))
+}
+
+func io_read_bit(channel int) {
+	C.io_read_bit(C.int(channel))
+}
+
+func io_read_analog(channel int) {
+	C.io_read_analog(C.int(channel))
+}
