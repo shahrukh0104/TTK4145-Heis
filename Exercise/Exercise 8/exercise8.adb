b62cd39c8ceaeba7e499114d3f345016fabb410a
Student <student@itk-d650>
2016-03-04 14:43:23 +0100

    Finished exercise 8 and it is approved aswell

--- Exercise/Exercise 8/exercise8.adb
+++ Exercise/Exercise 8/exercise8.adb
@@ -8,29 +8,22 @@ procedure exercise8 is
 
     protected type Transaction_Manager (N : Positive) is
         entry Finished;
-        function Commit return Boolean;
+        entry Wait_Until_Aborted;
         procedure Signal_Abort;
     private
         Finished_Gate_Open  : Boolean := False;
         Aborted             : Boolean := False;
-        Should_Commit       : Boolean := True;
     end Transaction_Manager;
     protected body Transaction_Manager is
         entry Finished when Finished_Gate_Open or Finished'Count = N is
         begin
             ------------------------------------------
             -- PART 3: Complete the exit protocol here
-            ------------------------------------------
-
+            -----------------------------------------
 
             if Finished'Count = N-1 then 
                 Finished_Gate_Open := True;
-                    if Aborted = True then
-                        Should_Commit := False;
-                        Aborted := False;
-                    else 
-                        Should_Commit := True;
-                    end if;
+
             end if;
             if Finished'Count = 0 then
                 Finished_Gate_Open := False;
@@ -39,15 +32,19 @@ procedure exercise8 is
 
         end Finished;
 
+        entry Wait_Until_Aborted when Aborted is
+        begin
+        if Wait_Until_Aborted'Count = 0 then
+            Aborted := False;
+
+        end if;
+        end Wait_Until_Aborted;
+
         procedure Signal_Abort is
         begin
             Aborted := True;
         end Signal_Abort;
 
-        function Commit return Boolean is
-        begin
-            return Should_Commit;
-        end Commit;
         
     end Transaction_Manager;
 
@@ -89,30 +86,29 @@ procedure exercise8 is
             Put_Line ("Worker" & Integer'Image(Initial) & " started round" & Integer'Image(Round_Num));
             Round_Num := Round_Num + 1;
 
-            ---------------------------------------
-            -- PART 2: Do the transaction work here             
-            ---------------------------------------
 
-            begin
-                Num := Unreliable_Slow_Add(Num);
-            exception
-                when Count_Failed =>
-                    Manager.Signal_Abort;
-            end;
-            Manager.Finished;
 
-            
-            if Manager.Commit = True then
-                Put_Line ("  Worker" & Integer'Image(Initial) & " comitting" & Integer'Image(Num));
-            else
-                Put_Line ("  Worker" & Integer'Image(Initial) &
-                             " reverting from" & Integer'Image(Num) &
-                             " to" & Integer'Image(Prev));
-                -------------------------------------------
-                -- PART 2: Roll back to previous value here
-                -------------------------------------------
-                Num := Prev;
-            end if;
+            select
+                Manager.Wait_Until_Aborted;
+                Num := Prev + 5;
+                Put_Line( "  Worker" & Integer'Image( Initial ) & ": forward recovery triggered," 
+                    & "changing from " & Integer'Image( Prev ) & " to " & Integer'Image( Num ) );
+            then abort
+                begin
+                    Num := Unreliable_Slow_Add( Num );
+                    Manager.Finished;
+                    Put_Line ("  Worker" & Integer'Image(Initial) & " comitting" & Integer'Image(Num));
+                
+                exception
+                    when Count_Failed => 
+                        begin
+                            Put_Line( "  Worker" & Integer'Image( Initial ) & " aborting" );
+                            Manager.Signal_Abort;
+                        end;
+                end;
+
+            end select;
+
 
             Prev := Num;
             delay 0.5;
@@ -128,4 +124,4 @@ procedure exercise8 is
 
 begin
     Reset(Gen); -- Seed the random number generator
-end exercise7;
+end exercise8;
